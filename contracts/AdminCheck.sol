59be37554de3a9d6afc68ac2032be6ec6e380ed4
Panachai-V <katooo241@gmail.com>
2021-11-10 18:37:21 +0700

    Edit check status

--- contracts/AdminCheck.sol
+++ contracts/AdminCheck.sol
@@ -4,17 +4,21 @@ pragma solidity ^0.8.2;
 import "./TokenGen.sol";
 import "./TicketMachine.sol";
 
-contract AdminChech{
+contract AdminCheck{
     TicketCtrl ticket;
 
      function checkStatus(uint _ticketid) public view returns(string memory) {
         ticket.getUsedStatus(_ticketid);
         if ( tkData.isUsed == true) {
-            string memory message = "This ticket is avilable";
+            string memory message = "This ticket is valid.";
+            return message;
+        }
+        else if ( tkData.isUsed == false) {
+            string memory message = "This ticket already used";
             return message;
         }
         else {
-            string memory message = "This ticket is unavilable";
+            string memory message = "This ticket is invalid";
             return message;
         }
      }

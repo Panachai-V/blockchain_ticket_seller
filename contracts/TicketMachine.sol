41a44a7193e4a4d7bfc4fba8c8b990122e0be576
Aoranun Rojsatitpong <91817113+aoranun@users.noreply.github.com>
2021-11-12 15:35:12 +0700

    New Edit Version

--- contracts/TicketMachine.sol
+++ contracts/TicketMachine.sol
@@ -6,39 +6,62 @@ import "./TokenGen.sol";
 contract TicketMachine{
      TicketCtrl ticket;
      uint tkprice = 4 ether;
+     uint public value;
+     address payable public seller;
+     address payable public buyer;
+
+     modifier onlySeller() {
+       require(msg.sender == seller,
+         "only seller can call.");
+         _;
+     }
+
+     modifier onlyBuyer() {
+       require(msg.sender == buyer,
+         "only buyer can call.");
+         _;
+     }
 
      function extra() public {
         ticket = new TicketCtrl();
      }
 
-    function getTicketID(string memory t_name,string memory s_detail,string memory date_detail) public view returns(string memory) {
+     function withdraw() public onlyBuyer {
+       // คนขายถอนเงินจากระบบ
+       buyer.transfer(address(this).balance);
+     }
+
+    function getTicketID(string memory t_name,string memory s_detail,string memory date_detail) public onlyBuyer view returns( ,uint tkID) {
       return ticket.getIDByNameAndDetail(t_name, s_detail, date_detail);
     }
 
-    function transferTK(address to_addr,uint t_id) {
+    function transferTK(address to_addr,uint t_id) public onlyBuyer returns {
       // User โอนสิทธิ์ความเป็นเจ้าของตั๋ว
       return ticket.transferTicket(to_addr, t_id);
     }
 
-    function getTKprice(string memory t_name,string memory s_detail,string memory date_detail) public view returns(string memory) {
+    function getTKprice(string memory t_name,string memory s_detail,string memory date_detail) public view returns(uint price) {
       // สรุปราคาของตั๋ว
-       _ticketid = getTicketID(t_name, s_detail, date_detail);
+       (, uint _ticketid) = getTicketID(t_name, s_detail, date_detail);
        return ticket.getPrice(_ticketid);
     }
 
-    function buyTicket(uint _ticketid, address payable _maker) public payable returns(address) {
-        // ตรวจสอบยอดการชำระเงิน และทำการโอนสิทธิ์ตั๋วให้กับ  User
+    function buyTicket(uint _ticketid, address _maker, address buyer) public onlyBuyer payable returns(address) {
+        // ตรวจสอบยอดการชำระเงิน และทำการโอนสิทธิ์ตั๋วให้กับ User
+        //tkprice = ticket.getPrice(_ticketid);
         if (msg.value < tkprice) {
-            revert("Not enough Ether!");
+            revert("Not enough Ether!!");
         }
         else {
             return ticket.transferFrom(_maker, buyer, _ticketid);
         }
     }
 
-    function requestRefund() {
-          // User ทำการขอเงินคืนจากระบบ
-        }
+    function requestRefund(uint _ticketid, address _maker, address buyer) public onlyBuyer returns(address) {
+        // User ทำการขอเงินคืน ขายตั๋วคืนระบบ
+        buyer.transfer(address(this).balance);
+        return ticket.transferFrom(buyer, _maker, _ticketid);
+    }
 
     function checkOwnership() {
         // ตรวจสอบสิทธิ์ความเป็นเจ้าของตั๋ว โดยใช้ Transaction hash

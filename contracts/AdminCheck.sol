60d3e202483e8f3531cc6c9fb1fffa328521ad32
Panachai-V <katooo241@gmail.com>
2021-11-20 14:33:44 +0700

    Edit check status and add comment

--- contracts/AdminCheck.sol
+++ contracts/AdminCheck.sol
@@ -5,31 +5,33 @@ import "./tokenGen.sol";
 
 
 contract AdminCheck{
-   // TicketCtrl ticket;
 
- 
-     function checkStatus(uint _ticketid) public view returns(string memory) {
-        TicketCtrl ticket;
-        bool status = ticket.getUsedStatus(_ticketid);
+    //ฟังค์ชันสำหรับเช็กสถานะการใช้ตั๋ว
+     function checkStatus(address _addr,uint _ticketid) public view returns(string memory) {
+        TicketCtrl t = TicketCtrl(_addr);
+        bool status = t.getUsedStatus(_ticketid);
+        //ถ้าตั๋วถูกใช้
         if ( status == true) {
-            string memory message = "This ticket is valid.";
+            string memory message = "This ticket already used.";
             return message;
         }
+        //ถ้าตั๋วถูกไม่ถูกใช้
         else if ( status == false) {
-            string memory message = "This ticket already used";
+            string memory message = "The ticket has not been used.";
             return message;
         }
+        //ถ้าตั๋วมีข้อผิดพลาดหรือยังไม่ถูกสร้าง
         else {
             string memory message = "This ticket is invalid";
             return message;
         }
      }
-
+    //เปลี่ยสถานะตั๋วในเป็นใช้แล้ว
     function doActivateTicket(address _addr,uint _ticketid) public payable {
      TicketCtrl t = TicketCtrl(_addr);
      t.pumpTimeStamp(_ticketid);
     }
-    
+    //เปลี่ยสถานะตั๋วในเป็นยังไม่ได้ใช้
     function undoActivateTicket(address _addr,uint _ticketid) public payable {
      TicketCtrl t = TicketCtrl(_addr);
      t.revertUsedStatus(_ticketid);
